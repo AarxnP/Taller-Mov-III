e82d30cf30649a1854289e33afd3529db3cf1cc8
AarxnP <aarxndobleaa@gmail.com>
2024-12-09 08:05:40 -0500

    taller2

--- lib/screens/bienvenida_screen.dart
+++ lib/screens/bienvenida_screen.dart
@@ -1,20 +1,112 @@
 import 'package:flutter/material.dart';
-import 'package:pelicula_01/navegadores/drawer.dart';
 
 class BienvenidaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pantalla de Bienvenida'),
-      ),
-      drawer: CustomDrawer(),
-      body: Center(
-        child: Text(
-          'Bienvenido a la aplicación',
-          style: TextStyle(fontSize: 24),
-          textAlign: TextAlign.center,
-        ),
+      body: Stack(
+        children: [
+          // Imagen de fondo desde un enlace
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://img.lovepik.com/background/20211021/large/lovepik-vintage-texture-background-image_401245438.jpg', // Cambia este enlace si lo necesitas
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Sombra para oscurecer el fondo
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black.withOpacity(0.7),
+                  Colors.black.withOpacity(0.4),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          // Contenido principal
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  '¡Bienvenido a ALLPeliculas!',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.black45,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Descubre una experiencia increíble con nuestra app. Gestiona tus películas favoritas y disfrútalas en cualquier momento, hora y lugar. ¡Estamos encantados de tenerte aquí!',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    backgroundColor: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    elevation: 5,
+                  ),
+                  child: const Text(
+                    'Iniciar Sesión',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    side: const BorderSide(color: Colors.white),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    'Registrarse',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,36 +1,111 @@
 import 'package:flutter/material.dart';
-import 'package:pelicula_01/navegadores/drawer.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Inicio de Sesión / Registro'),
-      ),
-      drawer: CustomDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Correo Electrónico'),
+      // appBar: AppBar(
+      //   title: const Text('Inicio de Sesión / Registro'),
+      //   backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      // ),
+      body: Stack(
+        children: [
+          // Imagen de fondo
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2021/05/30/19/52/abstract-rainbow-6296890_1280.jpg', // Reemplaza con tu URL de imagen
+                ),
+                fit: BoxFit.cover,
+              ),
             ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Contraseña'),
-              obscureText: true,
+          ),
+          // Contenido principal
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.account_circle,
+                    size: 100,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  const SizedBox(height: 32),
+                  const Text(
+                    'Bienvenido de nuevo',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.8),
+                      labelText: 'Correo Electrónico',
+                      labelStyle: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Color.fromARGB(255, 0, 0, 0), width: 2),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.8),
+                      labelText: 'Contraseña',
+                      labelStyle: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Color.fromARGB(255, 0, 0, 0), width: 2),
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Simulación de inicio de sesión exitoso
+                      Navigator.pushReplacementNamed(context, '/catalogo');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      textStyle: const TextStyle(fontSize: 18),
+                    ),
+                    child: const Text('Iniciar sesión'),
+                  ),
+                  const SizedBox(height: 16),
+                  TextButton(
+                    onPressed: () {
+                      // Lógica para registro
+                    },
+                    child: const Text(
+                      '¿No tienes cuenta? Regístrate',
+                      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Simulación de inicio de sesión exitoso
-                Navigator.pushReplacementNamed(context, '/catalogo');
-              },
-              child: Text('Iniciar sesión'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
